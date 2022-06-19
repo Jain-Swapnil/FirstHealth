b83c17c008ce6e492c3e77c047ea108a3db8af02
Swapnil Jain <76614640+Jain-Swapnil@users.noreply.github.com>
2022-06-19 17:47:36 +0530

    Update configure.php

--- Logic-Coders-Hackathon/configure.php
+++ Logic-Coders-Hackathon/configure.php
@@ -1,9 +1,9 @@
 <?php
 
-	define('DB_SERVER',"sql200.epizy.com");
-	define('DB_USERNAME',"epiz_31990349");
-	define('DB_PASSWORD',"1RwqXpf2sj458Ys");
-	define('DB_DATABASE',"epiz_31990349_logiccoders");
+	define('DB_SERVER',"localhost");
+	define('DB_USERNAME',"root");
+	define('DB_PASSWORD',"");
+	define('DB_DATABASE',"logiccoders");
 	
 	$connect = mysqli_connect(DB_SERVER,DB_USERNAME,DB_PASSWORD,DB_DATABASE);
 	if(mysqli_connect_errno())
